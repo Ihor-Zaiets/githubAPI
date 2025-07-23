91d989060cebe2a955b3dc6bbce440df7a4d788b
Ihor Zaiets <dekstroer8202@gmail.com>
2025-07-23 15:44:43 +0200

    exclude isFork field from response

--- src/main/java/com/ihorzaiets/githubapi/module/githubapi/dto/GithubRepositoryDTO.java
+++ src/main/java/com/ihorzaiets/githubapi/module/githubapi/dto/GithubRepositoryDTO.java
@@ -1,13 +1,14 @@
 package com.ihorzaiets.githubapi.module.githubapi.dto;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record GithubRepositoryDTO(String name, String ownerLogin, boolean isFork, List<GithubRepositoryBranchDTO> branches) {
+public record GithubRepositoryDTO(String name, String ownerLogin, @JsonIgnore boolean isFork, List<GithubRepositoryBranchDTO> branches) {
 
     @JsonCreator
     public GithubRepositoryDTO(
