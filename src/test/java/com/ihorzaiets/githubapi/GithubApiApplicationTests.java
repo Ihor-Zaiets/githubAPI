05deeed6d90d8799a9c87094bff43f00ca631a78
Ihor Zaiets <dekstroer8202@gmail.com>
2025-07-24 18:53:03 +0200

    update test, check for forked repository

--- src/test/java/com/ihorzaiets/githubapi/GithubApiApplicationTests.java
+++ src/test/java/com/ihorzaiets/githubapi/GithubApiApplicationTests.java
@@ -13,6 +13,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,14 +24,20 @@ class GithubApiApplicationTests {
 	@Autowired
 	private TestRestTemplate testRestTemplate;
 
+	/**
+	 * The test is written in a way to not have any mocks, therefore the call to GitHub api is not mocked.
+	 * <p>
+	 * The test relays on data from GitHub's test user "octocat". So, if any changed would happen with GitHub test data, test may stop working properly.
+	 * */
 	@Test
 	void applicationTest() {
 		//given
-		String testUsername = "octocat";
+		final String TEST_USERNAME = "octocat";
+		final String FORKED_REPOSITORY_NAME = "boysenberry-repo-1";
 
 		//when
 		ResponseEntity<List<GithubRepositoryDTO>> response = testRestTemplate.exchange(
-				"/api/github/getUserRepositoriesInfo/%s".formatted(testUsername),
+				"/api/github/getUserRepositoriesInfo/%s".formatted(TEST_USERNAME),
 				HttpMethod.GET,
 				null,
 				new ParameterizedTypeReference<>(){});
@@ -38,6 +46,9 @@ class GithubApiApplicationTests {
 		List<GithubRepositoryDTO> repositories = response.getBody();
 		assertEquals(HttpStatus.OK, response.getStatusCode());
 		assertNotNull(repositories);
+
+		Set<String> repoNameSet = repositories.stream().map(GithubRepositoryDTO::name).collect(Collectors.toSet());
+		assertFalse(repoNameSet.contains(FORKED_REPOSITORY_NAME));
 
 		for (GithubRepositoryDTO repo : repositories) {
 			assertNotNull(repo.name());
@@ -48,7 +59,7 @@ class GithubApiApplicationTests {
 			assertFalse(repo.ownerLogin().isBlank());
 			assertFalse(repo.branches().isEmpty());
 
-			assertEquals(repo.ownerLogin(), testUsername);
+			assertEquals(repo.ownerLogin(), TEST_USERNAME);
 
 			for (GithubRepositoryBranchDTO branch : repo.branches()) {
 				assertNotNull(branch.name());
